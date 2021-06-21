452e3335077cb3cfb41583fd92a96c12ece5e935
sammui4 <490454041@qq.com>
2021-06-21 23:11:01 +0800

    lib

---
+++ dart/lib/db.dart
@@ -0,0 +1,12 @@
+/*
+ * @Author: w
+ * @Date: 2021-06-21 21:31:52
+ * @LastEditors: w
+ * @LastEditTime: 2021-06-21 21:31:53
+ */
+abstract class Db{
+  String uri = '';
+  add(String data);
+  save();
+  delete();
+}
